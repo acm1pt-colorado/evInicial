d8acabd0ff7438b522308909cb925a1114430f1c
acm1pt-colorado <luisadanjimenez@gmail.com>
2016-02-01 12:00:17 +0100

    oauth2.0

--- config/passport.js
+++ config/passport.js
@@ -8,4 +8,17 @@ _.merge(exports, {
 
   // Extend with custom logic here by adding additional fields, methods, etc.
 
+  passport: {
+		google : {
+		    name: 'Google',
+		    protocol: 'oauth2',
+		    strategy: require('passport-google-oauth').OAuth2Strategy,
+		    options: {
+		      clientID: '328156475210-98crr3l1oef8ucimrj9sou0lb72arsgm.apps.googleusercontent.com',
+		      clientSecret: 'B-eANd6KroupdcKnGVqWgn-O',
+		      scope: ['profile', 'email'],
+		      hd: 'murciaeduca.es'
+		    }
+	  }
+	}
 });
